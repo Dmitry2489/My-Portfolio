b03ae1abc5228b5521d0e78da63d72e9812cfe01
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-05-20 20:26:35 +0300

    new project

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 //find the item
 const buttonBiography = document.querySelector("#button-biography");
 const buttonProject = document.querySelector("#button-projects");
@@ -8,12 +8,12 @@ const blockBio = document.querySelector(".bio");
 const btnMyWork = document.querySelector(".btn__my-work button");
 const btnMyContact = document.querySelector(".btn__my-contact button");
 const blockContact = document.querySelector(".contact");
-const preloaderBlock = document.querySelector('.preloader');
-const headerLogo = document.querySelector('.header__logo');
-const headerTitle = document.querySelector('.header__title');
-const headerText = document.querySelector('.header__text');
-const headerBtn = document.querySelector('.header__button-inner');
-const headerParallax = document.querySelector('.header__parallax-background');
+const preloaderBlock = document.querySelector(".preloader");
+const headerLogo = document.querySelector(".header__logo");
+const headerTitle = document.querySelector(".header__title");
+const headerText = document.querySelector(".header__text");
+const headerBtn = document.querySelector(".header__button-inner");
+const headerParallax = document.querySelector(".header__parallax-background");
 
 // window.onload = function () {
 //   alert('Страница загружена');
@@ -21,17 +21,17 @@ const headerParallax = document.querySelector('.header__parallax-background');
 //   // к этому моменту страница загружена
 //   alert(`Image size: ${img.offsetWidth}x${img.offsetHeight}`);
 // };
-window.addEventListener('load', () => {
-  window.setTimeout(() => {
-    preloaderBlock.classList.add('preloader__hide');
-    headerParallax.classList.add('lup-parallax');
-  }, 4000);
-  window.setTimeout(() => {
-    headerLogo.style.display = 'flex';
-    headerTitle.style.display = 'block';
-    headerText.style.display = 'block';
-    headerBtn.style.display = 'flex';
-  }, 3000);
+window.addEventListener("load", () => {
+    window.setTimeout(() => {
+        preloaderBlock.classList.add("preloader__hide");
+        headerParallax.classList.add("lup-parallax");
+    }, 4000);
+    window.setTimeout(() => {
+        headerLogo.style.display = "flex";
+        headerTitle.style.display = "block";
+        headerText.style.display = "block";
+        headerBtn.style.display = "flex";
+    }, 3000);
 });
 
 // Hang the event on the buttons
@@ -41,61 +41,76 @@ btnMyWork.addEventListener("click", onButtonMyWorkClick);
 btnMyContact.addEventListener("click", onButtonMyContactClick);
 
 function onButtonBiographyClick() {
-  blockHeader.classList.add("rotateSliderOutToRight");
-  blockBio.classList.add("rotateSliderInFromLeft");
+    blockHeader.classList.add("rotateSliderOutToRight");
+    blockBio.classList.add("rotateSliderInFromLeft");
 
-  var wrapper = document.querySelector(".wrapper__bio-section");
-  var text = document.querySelector(".text");
+    // let wrapper = document.querySelector(".about__info-inner");
+    // let text = document.querySelector(".about__info-inner p");
 
-  // var textCont = text.textContent;
-  // text.style.display = "none";
+    // let textCont = text.textContent;
+    // text.style.display = "none";
 
-  // for (var i = 0; i < textCont.length; i++) {
-  //   (function(i) {
-  //     setTimeout(function() {
-  //       // Created textNode to append
-  //       var texts = document.createTextNode(textCont[i]);
-  //       var span = document.createElement("span");
-  //       span.appendChild(texts);
+    // for (let i = 0; i < textCont.length; i++) {
+    //     (function (i) {
+    //         setTimeout(function () {
+    //             // Created textNode to append
+    //             let texts = document.createTextNode(textCont[i]);
+    //             let span = document.createElement("span");
+    //             span.appendChild(texts);
 
-  //       span.classList.add("wave");
-  //       wrapper.appendChild(span);
-  //     }, 75 * i);
-  //   })(i);
-  // }
+    //             // span.classList.add("wave");
+    //             wrapper.appendChild(span);
+    //         }, 75 * i);
+    //     })(i);
+    // }
+
+    // let textBio = document.querySelectorAll(".textAppeared");
+
+    // // textBio[0].innerText;
+    // debugger;
+    // for (let i = 0; i < textBio[0].innerText.length; i++) {
+    //     // textBio[0].innerText[i][i].style.display = "none";
+    //     console.log(textBio[0].innerText[i]);
+    // }
+
+    // let dima = textBio[0].innerText;
+    // console.log(dima);
+
+    // console.log(textBio);
+    // console.log(textBio[0].innerText[0]);
 }
 
 function onButtonProjectClick() {
-  blockHeader.classList.add("rotateSliderOutToLeft");
-  blockProject.classList.add("rotateSliderInFromRight");
+    blockHeader.classList.add("rotateSliderOutToLeft");
+    blockProject.classList.add("rotateSliderInFromRight");
 }
 
 function onButtonMyWorkClick() {
-  blockBio.classList.remove("rotateSliderInFromLeft");
-  blockBio.classList.add("rotateFall");
-  blockProject.classList.remove("rotateSliderInFromRight");
-  blockProject.classList.add("scaleUp");
+    blockBio.classList.remove("rotateSliderInFromLeft");
+    blockBio.classList.add("rotateFall");
+    blockProject.classList.remove("rotateSliderInFromRight");
+    blockProject.classList.add("scaleUp");
 }
 
 function onButtonMyContactClick() {
-  blockProject.classList.remove("rotateSliderInFromRight");
-  blockProject.classList.remove("scaleUp");
-  blockProject.classList.add("rotateFall");
-  blockContact.classList.add("scaleUp");
+    blockProject.classList.remove("rotateSliderInFromRight");
+    blockProject.classList.remove("scaleUp");
+    blockProject.classList.add("rotateFall");
+    blockContact.classList.add("scaleUp");
 }
 
 function parallax(event) {
-  const parallaxSelector = document.querySelector(
-    ".header__parallax-background"
-  );
-  parallaxSelector.style.transform =
-    "translate(" +
-    event.clientX / 20 +
-    "px" +
-    "," +
-    event.clientY / 20 +
-    "px" +
-    ")";
+    const parallaxSelector = document.querySelector(
+        ".header__parallax-background"
+    );
+    parallaxSelector.style.transform =
+        "translate(" +
+        event.clientX / 20 +
+        "px" +
+        "," +
+        event.clientY / 20 +
+        "px" +
+        ")";
 }
 // translate(x,y)
 
